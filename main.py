d8620e5b47bfdecfddf827ec82b83dc980a2d612
Eli Jordan <elijordan282@gmail.com>
2024-02-12 18:39:18 -0700

    minimum product
    Launches a GUI with a list of employees from coffee_history.csv. The user can select who is getting coffee that day and updates the .csv file with the amount spent. Decision on who pays is who has paid the least total amount as stored in the csv file. Video to come

--- main.py
+++ main.py
@@ -7,83 +7,74 @@ import tkinter as tk
 import customtkinter as ctk
 
 ctk.set_appearance_mode("System")
+_DEBUG = True
 
-employees_df = pd.read_csv('coffee_history.csv')
+class App(ctk.CTk):
+    employees_df = pd.read_csv('coffee_history.csv')
 
-root = ctk.CTk()
-root.grid_rowconfigure(2,weight=1)
-root.grid_columnconfigure(2,weight=1)
+    def __init__(self):
+        super().__init__()
+        self.geometry("400x300")
+        self.title("Whose Turn is it Anyways?")
+        self.resizable(False, False)
+        self.grid_rowconfigure(2, weight=1)
+        self.grid_columnconfigure(2, weight=1)
 
-root.geometry("500x500")
-root.title("Whose Turn is it Anyways?")
+        self.instantiate_listbox()
 
-root.resizable(False, False)
+        self.employee_chosen_output = ctk.CTkTextbox(self)
+        self.employee_chosen_output.grid(column=0, row=1, sticky="")
 
-listbox = tk.Listbox(root, selectmode="multiple")
-listbox.pack(fill=tk.BOTH, expand=False, padx=5, pady=5)
-listbox.grid(column=0, row=0, sticky="nsew")
+        self.employees_selected = []
+        self.button = ctk.CTkButton(self, text="Stir the Pot", command=self.button_callback)
+        self.button.grid(column=1, row=1, sticky="ew")
 
-i = 0
-for employee in employees_df.loc[:, "Employee"]:
-    listbox.insert(i, employee)
-    i += 1
+        self.selected_employee = ""
+    def button_callback(self):
+        self.employee_chosen_output.delete("0.0", ctk.END)
+        self.selected_employee = self.employees_df[self.employees_df["Dollars Spent"] == min(self.employees_df.loc[list(self.listbox.curselection()), "Dollars Spent"])].loc[:, "Employee"].values[0]
+        self.employee_chosen_output.insert("0.0", self.selected_employee)
+        self.sum_coffee_order(list(self.listbox.curselection()))
 
-employee_chosen_output = ctk.CTkTextbox(root)
-employee_chosen_output.grid(column=0, row=1, sticky="")
+    def instantiate_listbox(self):
+        self.listbox = tk.Listbox(self, selectmode="multiple")
+        self.listbox.pack(fill=tk.BOTH, expand=False, padx=5, pady=5)
+        self.listbox.grid(column=0, row=0, sticky="nsew")
 
-# what are these references idk what's going on
-# def choose_employee():
-#     try:
-#         print(min(employees_df.loc[list(listbox.curselection()), "Dollars Spent"]))
-#     except ValueError:
-#         print("Value Error")
-#         employee_chosen_output.delete("0.0", ctk.END)
-#     else:
-#         print("else")
-#         employee_chosen_output.delete("0.0", ctk.END)
-#         employee_chosen_output.insert("0.0", employees_df[employees_df["Dollars Spent"] == min(employees_df.loc[list(listbox.curselection()), "Dollars Spent"])].loc[:,"Employee"].values[0])
+        i = 0
+        for employee in self.employees_df.loc[:, "Employee"]:
+            self.listbox.insert(i, employee)
+            i += 1
 
-def sum_coffee_order(employees:list):
-    coffee_prices = {
-        "Black": 3.0,
-        "Mocha": 5.0,
-        "Frappe": 5.50,
-        "Cappuccino": 4.50,
-        "Americano": 4.0,
-        "Tea": 3.50,
-        "Soda": 4.25
-    }
+    def sum_coffee_order(self, employees: list):
+        coffee_prices = {
+            "Black": 3.0,
+            "Mocha": 5.0,
+            "Frappe": 5.50,
+            "Cappuccino": 4.50,
+            "Americano": 4.0,
+            "Tea": 3.50,
+            "Soda": 4.25
+        }
 
-    coffee_prices = pd.DataFrame.from_dict(coffee_prices, orient="index",columns=["coffee_price"])
-    #define the coffees we need
-    coffee_total=0
-    coffee_order = employees_df.loc[employees, "Favorite Drink"]
-    for price in coffee_prices.loc[coffee_order]:
-        coffee_total += int(price)
-
-    employees_df.loc[employee_chosen_output.get("0.0"), "Dollars Spent"] += coffee_total
-
-    print(coffee_total)
-
-# button lambda function necessary because the listbox.curselection() returns null when empty
-# I am so sorry for this lambda function
-#I'm going to clean this up
-# maybe
-
-employees_selected = []
-button = ctk.CTkButton(root, text="Stir the Pot", command=
-lambda: (
-    employee_chosen_output.delete("0.0",ctk.END),
-    employee_chosen_output.insert("0.0", employees_df[employees_df["Dollars Spent"] == min(employees_df.loc[list(listbox.curselection()), "Dollars Spent"])].loc[:, "Employee"].values[0]),
-    sum_coffee_order(list(listbox.curselection()))
-))
-
-
-button.grid(column=1, row=1, sticky="ew")
-
+        # coffee_prices = pd.DataFrame.from_dict(coffee_prices, orient="index",columns=["coffee_price"])
+        # define the coffees we need
+        coffee_total = 0
+        coffee_order = list(self.employees_df.loc[employees, "Favorite Drink"])
+        if _DEBUG:
+            print(coffee_order)
+        for coffee in coffee_order:
+            coffee_total += coffee_prices[coffee]
+        if _DEBUG:
+            print(coffee_total)
+        self.employees_df.loc[self.employees_df[self.employees_df["Employee"] == self.selected_employee].index.values[0], "Dollars Spent"] += coffee_total
+        if _DEBUG:
+            print(self.employees_df.loc[self.employees_df[self.employees_df["Employee"] == self.selected_employee].index.values[0], "Dollars Spent"])
+        self.employees_df.to_csv("coffee_history.csv", index=False)
 
 
 if __name__ == '__main__':
-    root.mainloop()
+    app = App()
+    app.mainloop()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
