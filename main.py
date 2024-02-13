49de37a1db63ef7e2b9a27bdca006e3b19be93ba
Eli Jordan <elijordan282@gmail.com>
2024-02-13 00:13:42 -0700

    swapped textbox for label

--- main.py
+++ main.py
@@ -20,35 +20,50 @@ class App(ctk.CTk):
         self.geometry("400x300")
         self.title("Whose Turn is it Anyways?")
         self.resizable(False, False)
-        self.grid_rowconfigure(2, weight=1)
+        self.grid_rowconfigure(1, weight=1)
         self.grid_columnconfigure(2, weight=1)
 
         self.instantiate_listbox()
+        self.frame = ctk.CTkFrame(self)
+        self.frame.grid(row=0, column=2, sticky="nsew")
+
         # add names to listbox
-        self.employee_chosen_output = ctk.CTkTextbox(self)
-        self.employee_chosen_output.grid(column=0, row=1, sticky="")
+        self.employee_chosen_output = ctk.CTkLabel(self.frame, text="Employee")
+        self.employee_chosen_output.configure(font=("Helvetica", 30), text_color="#652A0E")
+        self.employee_chosen_output.grid(column=2, row=0, sticky="nsew")
 
         # create button
-        self.button = ctk.CTkButton(self, text="Stir the Pot", command=self.button_callback)
-        self.button.grid(column=1, row=1, sticky="ew")
+        self.button = ctk.CTkButton(self, text="Stir the Pot",fg_color="#652A0E", command=self.button_callback)
+        self.button.grid(column=2, row=1, sticky="nsew")
 
         # reference for employee selected
         self.selected_employee = ""
 
     def button_callback(self):
-        # clear textbox
-        self.employee_chosen_output.delete("0.0", ctk.END)
+
         # find employee with least spent
-        self.selected_employee = self.employees_df[self.employees_df["Dollars Spent"] == min(self.employees_df.loc[list(self.listbox.curselection()), "Dollars Spent"])].loc[:, "Employee"].values[0]
-        # update text box
-        self.employee_chosen_output.insert("0.0", self.selected_employee)
+        try:
+            self.selected_employee = self.employees_df[self.employees_df["Dollars Spent"] == min(self.employees_df.loc[list(self.listbox.curselection()), "Dollars Spent"])].loc[:, "Employee"].values[0]
+            self.write_to_textbox(self.selected_employee)
+        except:
+            self.write_to_textbox("No employee selected")
+
         # sum employees' coffee order
         self.sum_coffee_order(list(self.listbox.curselection()))
+
+    def write_to_textbox(self, text:str):
+        # # clear textbox
+        # self.employee_chosen_output.configure(state=ctk.NORMAL)
+        # self.employee_chosen_output.delete("0.0", ctk.END)
+        # # update text
+        # self.employee_chosen_output.insert("0.0", text)
+        # self.employee_chosen_output.configure(state=ctk.DISABLED)
+        self.employee_chosen_output.configure(text=text)
 
     def instantiate_listbox(self):
         self.listbox = tk.Listbox(self, selectmode="multiple")
         self.listbox.pack(fill=tk.BOTH, expand=False, padx=5, pady=5)
-        self.listbox.grid(column=0, row=0, sticky="nsew")
+        self.listbox.grid(column=0, row=0, rowspan=2, sticky="nsew")
 
         i = 0
         for employee in self.employees_df.loc[:, "Employee"]:
